c51ec9b9860af2a3f48cba0d237b39ff7af2da47
cbae <xchris.bae@gmail.com>
2023-03-23 22:07:25 -0700

    added workday hours and date

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,18 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// important BOM elements
+var currentDayEl = $('#currentDay');
+
+//display day
+function displayTime() {
+  var rightNow = dayjs().format('dddd, MMMM D');
+  currentDayEl.text(rightNow);
+}
+
+displayTime();
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
