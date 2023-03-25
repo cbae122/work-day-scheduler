3f6dc39a1a8cff16b292dad6bf7b6c609be63511
cbae <xchris.bae@gmail.com>
2023-03-24 18:33:49 -0700

    created var for timelock and creating function for timeblock

--- Develop/script.js
+++ Develop/script.js
@@ -12,7 +12,6 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -21,6 +20,21 @@ var currentTime = dayjs().hour()
 console.log(currentTime);
 
 // important BOM elements
+
+// //display day
+// var currentDayEl = $('#currentDay');
+// function displayTime() {
+//   var rightNow = dayjs().format('dddd, MMMM D');
+//   currentDayEl.text(rightNow);
+// };
+// displayTime();
+
+var timeBlocks = $('.time-block');
+
+timeBlocks.each(function () {
+  console.log(this.id);
+})
+
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -36,17 +50,11 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-
-$ ()
-
-
-
-
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
 
-$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
+$('#currentDay').text(dayjs().format('dddd, MMMM D YYYY'));
 });
