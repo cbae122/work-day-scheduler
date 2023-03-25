f2378964b1ead123c219c1a905b4bf96d31200a8
cbae <xchris.bae@gmail.com>
2023-03-24 23:56:19 -0700

    additional lines of getitem localstorage added for other hours

--- Develop/script.js
+++ Develop/script.js
@@ -61,6 +61,15 @@ $('.saveBtn').click(function (event) {
 
 //getting data from local storage
 $('#hour-9 .time-block').val(localStorage.getItem('9'));
+$('#hour-10 .time-block').val(localStorage.getItem('10'));
+$('#hour-11 .time-block').val(localStorage.getItem('11'));
+$('#hour-12 .time-block').val(localStorage.getItem('12'));
+$('#hour-13 .time-block').val(localStorage.getItem('13'));
+$('#hour-14 .time-block').val(localStorage.getItem('14'));
+$('#hour-15 .time-block').val(localStorage.getItem('15'));
+$('#hour-16 .time-block').val(localStorage.getItem('16'));
+$('#hour-17.time-block').val(localStorage.getItem('17'));
+
 
 // $('#currentDay').text(dayjs().format('dddd, MMMM D YYYY'));
 
